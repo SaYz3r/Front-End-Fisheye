34ac8bb974403e3ff484c02eb2219ae7c96daeb8
SaYz3r <hkoolenn@gmail.com>
2025-11-12 20:06:25 +0100

    Modification modal
    Modification du modal sur son placement ainsi que ca fermeture.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,25 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "flex"; // Utiliser flex pour centrer
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+// Fermer le modal en cliquant en dehors
+window.onclick = function(event) {
+    const modal = document.getElementById("contact_modal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+  // Vérifie si la touche pressée est "Escape" et si la modal est visible
+  if (event.key === "Escape" && contact_modal.style.display === "flex") {
+    // Si on est sur la page de confirmation, vider le formulaireS
+      closeModal();
+  }
+});
